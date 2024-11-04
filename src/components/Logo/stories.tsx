fb1682c8bfc3866eacd63daec619cb0133c169c7
Davi Costa <davim.costa@outlook.com>
2024-11-04 19:43:12 -0300

    finishing heading and logo components

---
+++ src/components/Logo/stories.tsx
@@ -0,0 +1,15 @@
+import { Meta, StoryObj } from '@storybook/react';
+import Logo, { LogoProps } from '.';
+
+export default {
+  title: 'Logo',
+  component: Logo,
+} as Meta<typeof Logo>;
+
+export const Default: StoryObj<LogoProps> = {
+  args: {
+    color: 'white',
+    size: 'normal',
+    $hideOnMobile: false,
+  },
+};
