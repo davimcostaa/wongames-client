ac3cf337f42c59c16f9bc081ae9852e431935b17
Davi Costa <davim.costa@outlook.com>
2024-11-21 20:00:30 -0300

    gamecard slider component

---
+++ src/components/GameCardSlider/stories.tsx
@@ -0,0 +1,67 @@
+import { Meta, StoryObj } from '@storybook/react';
+import GameCardSlider from '.';
+
+const items = [
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x140',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x141',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x142',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x143',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x144',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+  {
+    title: 'Population Zero',
+    developer: 'Rockstar Games',
+    img: 'https://source.unsplash.com/user/willianjusten/300x145',
+    price: 'R$ 235,00',
+    promotionalPrice: 'R$ 215,00',
+  },
+];
+
+export default {
+  title: 'GameCardSlider',
+  args: { items },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'dark',
+    },
+  },
+  component: GameCardSlider,
+} as Meta;
+
+export const Default: StoryObj = {
+  render: (args) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <GameCardSlider {...args} />
+    </div>
+  ),
+};
