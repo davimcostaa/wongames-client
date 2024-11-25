ab18ce4becddc8dc1290fcead03e1ef463427e99
Davi Costa <davim.costa@outlook.com>
2024-11-25 16:42:51 -0300

    adding minimal prop in button

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -52,6 +52,26 @@ describe('<Button />', () => {
     expect(screen.getByTestId('icon')).toBeInTheDocument();
   });
 
+  it('should render a minimal version', () => {
+    renderWithTheme(
+      <Button minimal icon={<AddShoppingCart data-testid="icon" />}>
+        Buy Now
+      </Button>,
+    );
+
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyle({
+      background: 'none',
+      color: '#F231A5',
+    });
+    expect(screen.getByRole('button', { name: /buy now/i })).toHaveStyleRule(
+      'background',
+      'none',
+      {
+        modifier: ':hover',
+      },
+    );
+  });
+
   it('should render Button as a Link', () => {
     renderWithTheme(
       <Button as="a" href="/game/1">
