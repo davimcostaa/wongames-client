b304777535723ae92b8db9117b48ca4184f8edbd
Davi Costa <davim.costa@outlook.com>
2024-12-11 17:41:50 -0300

    profilemenu component

---
+++ src/components/ProfileMenu/stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ProfileMenu, { ProfileMenuProps } from '.';
+
+export default {
+  title: 'ProfileMenu',
+  component: ProfileMenu,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark',
+    },
+  },
+} as Meta;
+
+export const Default: StoryObj<ProfileMenuProps> = {};
